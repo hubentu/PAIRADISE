858c0393f36663beca7337aede5090ed62fc830f
Qiang Hu <qiang.hu@roswellpark.org>
2018-12-19 10:45:44 -0500

    add two sample datasets, CEU and LUSC

---
+++ R/sample_dataset_CEU.R
@@ -0,0 +1,16 @@
+#' sample_dataset_CEU
+#' @docType data
+#' @format The dataset has 7 columns, arranged as follows:
+#' \describe{
+#' \item{ExonID}{Column 1 contains the ID of the alternative splicing events.}
+#' \item{I1}{Column 2 contains counts of isoform 1 corresponding to the first group.}
+#' \item{S1}{Column 3 contains counts of isoform 2 corresponding to the first group.}
+#' \item{I2}{Column 4 contains counts of isoform 1 corresponding to the second group.}
+#' \item{S2}{Column 5 contains counts of isoform 2 corresponding to the second group.}
+#' \item{I_len}{Column 6 contains the effective length of isoform 1.}
+#' \item{S_len}{Column 7 contains the effective length of isoform 2.}
+#' }
+#' @rdname sample_dataset
+#' @name sample_dataset_CEU
+#' @usage data(sample_dataset_CEU)
+NULL
